37a64bc9a9d31a042db99bf4e8d71d70d37fd72d
pisiliang <2282561186@qq.com>
2023-07-11 13:39:15 +0800

    gen

---
+++ backend/mouse/mouse-model/mouse-model-vueadmin/src/main/java/com/ppl/vueadmin/VueAdminStart.java
@@ -0,0 +1,17 @@
+package com.ppl.vueadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class VueAdminStart
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(VueAdminStart.class, args);
+    }
+}
---
+++ backend/mouse/mouse-model/mouse-model-vueadmin/src/main/java/com/ppl/vueadmin/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.ppl.vueadmin.controller;
+
+/**
+ * Created by pisiliang on 2023/7/11 9:49
+ */
+public class UserController {
+}
